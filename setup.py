d4b1945f4297dbbcf61434a4c321906017423db6
Marin Atanasov Nikolov <dnaeon@gmail.com>
2013-05-27 15:21:43 +0300

    Add the setup.py file to the repository

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+setup(name='pkglib',
+      version='0.0.1',
+      description='Python wrappers for libpkg',
+      author='Marin Atanasov Nikolov',
+      author_email='dnaeon@gmail.com',
+      license='BSD',
+      cmdclass = {'build_ext': build_ext},
+      ext_modules = [Extension("pkg", ["src/pkg.pyx"],
+                        include_dirs=['/usr/local/include'],
+                        library_dirs=['/usr/local/lib'],
+                        libraries=['pkg'],
+                    )]
+)
+
