efd53397b0d4fa48abc98c90f41f4622c76023c2
Marin Atanasov Nikolov <dnaeon@gmail.com>
2013-06-02 21:13:35 +0300

    Add docstrings to the module exceptions

--- src/pkg-exc.py
+++ src/pkg-exc.py
@@ -25,23 +25,63 @@
 #
 
 class PkgAlreadyInitialized(Exception):
+    """
+    Indicates that the library has already been initialized.
+
+    Raised if a consequent initialization to the library is requested,
+    while it has already been initiliazed.
+
+    """
     pass
 
 class PkgNotInitialized(Exception):
+    """
+    Indicates that the library is not initiliazed for use.
+
+    Raised when accessing library functions, but
+    the library has not been initialized or failed to do so.
+
+    """
     pass
 
 class PkgJobsAddError(Exception):
+    """
+    Error occurred while trying to add packages to a jobs object.
+
+    """
     pass
     
 class PkgJobsSolveError(Exception):
+    """
+    Error occurred while trying to solve package jobs.
+
+    """
     pass
     
 class PkgJobsApplyError(Exception):
+    """
+    Error occurred while trying to apply package jobs.
+
+    """
     pass
 
 class PkgAccessError(Exception):
+    """
+    Error indicating a permissions issue.
+
+    Raised when trying to access certain functionallity of the
+    library, but lack of permissions to do so has been detected.
+
+    """
     pass
 
 class PkgTypeError(Exception):
+    """
+    Error indicating access to attribute which is not specific for the package.
+
+    Raised when requesting an attribute of a package for which the package type
+    is not applicable, e.g. requesting the package repository for a local package file.
+
+    """
     pass
 
